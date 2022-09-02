6732bf0e797ec199dbcb85085dcf7c73c1745124
zhLisha <lisazhou1709@gmail.com>
2022-09-02 14:59:22 +0200

    Created Structure of Front End

---
+++ resources/js/views/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <a href="/admin">Admin privata</a>
+        <h1>Ciao questa e' la pagina App.vue</h1>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
@@ -26,4 +22,9 @@ Route::middleware('auth')
 ->prefix('admin')
 ->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
+});
+
+
+Route::get('{any?}', function () {
+    return view('guest.home');
 });
