9c24593797dca3b665b7f9c7f32048b00581cb5b
zhLisha <lisazhou1709@gmail.com>
2022-09-07 14:23:10 +0200

    Created migration new table: Categories, new column category_id(FK) to table Posts

---
+++ database/migrations/2022_09_07_115223_add_category_id_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // // Creo la nuova colonna
+            // $table->unsignedBigInteger('category_id')->nullable()->after('slug');
+
+            // // Assegno la FK
+            // $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Cancello prima la relazione
+            // $table->dropForeign('posts_category_id_foreign');
+
+            // Cancello la colonna
+            $table->dropColumn('category_id');
+        });
+    }
+}
